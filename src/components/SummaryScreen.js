f687d56adf148d6a7059e6a52981c95cae7f9c2d
sumith <sumith95023@gmail.com>
2024-06-10 23:08:29 +0530

    up..

--- src/components/SummaryScreen.js
+++ src/components/SummaryScreen.js
@@ -11,7 +11,9 @@ const SummaryScreen = ({ totalAttempts, totalElapsedTime, handlePlayAgain }) => 
       backgroundPosition: 'center',
       backgroundRepeat: 'repeat'
     }}>
-      <h1 className="summary-heading" style={{ textAlign: 'center', fontFamily: 'fantasy' }}>Sentence Verification Summary</h1>
+      <h1 className="summary-heading" style={{ textAlign: 'center', fontFamily: 'fantasy' }}>Sentence Verification Global</h1>
+      <h3 className="summary-heading" style={{ textAlign: 'center', fontFamily:'sans-serif', textDecoration: 'UnderLine', fontWeight:'bold'}}>Session-1 Stats</h3>
+
       <div className="summary-details" style={{ textAlign: 'center', marginTop: '20px' }}>
         <p>Total Attempts: {totalAttempts}</p>
         <p>Time Taken: {minutes}:{seconds < 10 ? `0${seconds}` : seconds} minutes</p>
